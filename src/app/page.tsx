a11c515031f3b2afe87a6b52989f0a76cd5ae337
Pedro Figueiredo <pedrohfm2012@yahoo.com.br>
2023-11-24 22:44:00 -0300

    modificando url da imagem de fundo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useSerie } from "@/utils/queries";
 const Page = () => {
   const serie = useSerie(84958);
   return (
-    <main className="h-screen lg:bg-cover cel:bg-cover cel:bg-center lg:bg-top bg-no-repeat bg-fixed " style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${serie.data?.seasons[0]?.poster_path}`}}>
+    <main className="h-screen lg:bg-cover cel:bg-cover cel:bg-center lg:bg-center bg-no-repeat bg-fixed " style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${serie.data?.poster_path}`}}>
       <div className="bg-black/70 h-screen">
         <div className="lg:flex lg:justify-center lg:items-center container font-b612 lg:mx-auto">
           <FeaturedSeries />
